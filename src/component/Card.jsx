11bb182a5c0ad6dbc419effe84adde40062d66cc
Thuyatun <thuratundev@gmail.com>
2024-03-03 23:28:57 +0630

    Conversion from Kelvin to Celsius

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Clock from 'react-clock';
-import { useState,useEffect } from "react";
-import 'react-clock/dist/Clock.css';
+import Clock from "react-clock";
+import { useState, useEffect } from "react";
+import "react-clock/dist/Clock.css";
 
 const Card = ({ data }) => {
   const [value, setValue] = useState(new Date());
@@ -13,6 +13,11 @@ const Card = ({ data }) => {
       clearInterval(interval);
     };
   }, []);
+
+  // Conversion from Kelvin to Celsius
+  const kelvinToCelsius = (kelvin) => {
+    return Math.round(kelvin - 273.15);
+  };
 
   if (JSON.stringify(data) !== "{}") {
     return (
@@ -26,19 +31,19 @@ const Card = ({ data }) => {
             src={`https://openweathermap.org//img/w/${data.weather[0].icon}.png`}
             alt="icon"
           />
-          {data.main.temp}°C
+          {kelvinToCelsius(data.main.temp)}°C
         </h2>
         <p>{data.weather[0].description}</p>
         <p>Humidity:{data.main.humidity}%</p>
         <p>Visibility:{data.visibility / 1000}km</p>
       </div>
     );
-  }else{
+  } else {
     return (
       <div>
         <h2 className="text-danger text-center my-4">Loading.....</h2>
       </div>
-    )
+    );
   }
 };
 
